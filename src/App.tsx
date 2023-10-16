20d5d5d17e3d996e8bc18f5ed35d7c1747c346d2
jahidulsaeid <jahidul.saeid@gmail.com>
2023-10-17 00:43:44 +0600

    🐛 Fix Bug - responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import draw from "./assets/images/draw.svg";
 import typeIcon from "./assets/images/type.svg";
 function App() {
   return (
-    <div className="container mx-auto my-10">
+    <div className="container mx-auto my-10 px-2 lg:px-14">
       <h1 className="text-4xl text-center my-5">Online Signature Generator</h1>
       <p className="text-center text-xl text-gray-500">
         Create and download your free e-signature
       </p>
 
-      <section className="mt-10 flex gap-10">
-        <div className="w-1/2 bg-blue-500 p-10 text-center rounded-lg">
+      <section className="mt-10 flex gap-10 flex-col md:flex-row px-2">
+        <div className="w-full md:w-1/2 bg-blue-500 px-5 md:px-10 py-10 text-center rounded-lg">
           <img src={draw} alt="DRAW" className="inline-block mb-5" />
           <h2 className="text-2xl font-bold text-white mb-5">
             Draw your signature
@@ -20,12 +20,12 @@ function App() {
           </p>
           <Link
             to="/draw"
-            className="mt-5 bg-white hover:bg-blue-500 text-blue-500 py-4 px-10 rounded-full border border-white hover:text-white font-medium text-lg inline-block"
+            className="mt-5 bg-white hover:bg-blue-500 text-blue-500 py-4 px-4 md:px-10 rounded-full border border-white hover:text-white font-medium text-lg inline-block"
           >
             Draw your signature
           </Link>
         </div>
-        <div className="w-1/2 bg-blue-500 p-10 text-center rounded-lg">
+        <div className="w-full md:w-1/2 bg-blue-500 px-5 md:px-10 py-10 text-center rounded-lg">
           <img src={typeIcon} alt="sign" className="inline-block mb-5" />
           <h2 className="text-2xl font-bold text-white mb-5">
             Type your signature
@@ -36,7 +36,7 @@ function App() {
           </p>
           <Link
             to="/type"
-            className="mt-5 bg-white hover:bg-blue-500 text-blue-500 py-4 px-10 rounded-full border border-white hover:text-white font-medium text-lg inline-block"
+            className="mt-5 bg-white hover:bg-blue-500 text-blue-500 py-4 px-4 md:px-10 rounded-full border border-white hover:text-white font-medium text-lg inline-block"
           >
             Type your signature
           </Link>
--- src/components/CanvasSignature.tsx
+++ src/components/CanvasSignature.tsx
@@ -1,7 +1,15 @@
+import { useEffect, useState } from "react";
 import SignatureCanvas from "react-signature-canvas";
 
 export default function CanvasSignature() {
   let sigPad = null as SignatureCanvas | null;
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setWidth(window.innerWidth);
+    });
+  }, []);
 
   return (
     <>
@@ -9,10 +17,14 @@ export default function CanvasSignature() {
         ref={(ref) => {
           sigPad = ref;
         }}
-        canvasProps={{ width: 800, height: 200, className: "signature_pad" }}
+        canvasProps={{
+          width: width < 768 ? width - 50 : width / 2 - 50,
+          height: 200,
+          className: "signature_pad",
+        }}
       />
 
-      <div className="canvas-button flex gap-5 items-center justify-center my-10">
+      <div className="canvas-button flex gap-5 items-center my-10 flex-col md:flex-row justify-center">
         <button
           className="bg-blue-500 hover:bg-blue-700 py-2 px-5 text-white rounded-full inline-block"
           onClick={() => sigPad?.clear()}
